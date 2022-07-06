33d26e76e6460f9dd31c5f30832477e9e12af419
gitgroch <geoff.rochford@gmail.com>
2022-07-06 13:30:01 +0000

    Add calculates surplus data to surplus worksheet

--- run.py
+++ run.py
@@ -56,7 +56,7 @@ def validate_data(values):
 
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with th elist data provided 
+    Update sales worksheet, add new row with the list data provided 
     """
     print("Updating sales worksheet...\n")
 
@@ -83,6 +83,16 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, from calculate surplus data
+    """
+    print("Updating surplus worksheet...\n")
+
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated succesfully!\n")
+
 
 def main():
     """
@@ -93,7 +103,7 @@ def main():
     update_sales_worksheet(sales_data)
     calculate_surplus_data(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
